8cc6d5dbcf67975b4ebbc58fb532722f4646c1f6
ffyuanda <46557895+ffyuanda@users.noreply.github.com>
2021-10-08 21:07:09 +0800

    feat: sh files
    Signed-off-by: ffyuanda <46557895+ffyuanda@users.noreply.github.com>

---
+++ dev_init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# envs
+export FLASK_ENV=development
+export FLASK_APP=app
+
+# venv
+deactivate
+
+if [ ! -d "./venv" ]
+then
+  python3 -m venv venv
+fi
+
+source ./venv/bin/activate
+
+# dependencies
+pip install -r requirements.txt
+echo Dependenceis installed!
+
+# init database and run
+# echo Trying to init database...
+# flask init-db
+
+# launch app using gunicorn
+flask run
